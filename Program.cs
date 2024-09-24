701710f0acde757f962fde746b81f9fdd3672ba7
lukas citora <lukas007c@seznam.cz>
2024-09-24 12:45:47 +0200

    init

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GoogleSearchApp;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+// Pøidání HttpClient s základní adresou (pokud je potøeba)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Pokud používáte serverovou aplikaci, nemusí být potøeba WebAssemblyHostBuilder
 
 await builder.Build().RunAsync();
