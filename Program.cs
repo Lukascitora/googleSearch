97876417877f1bf12d6451f85cc27c87c6772553
lukas citora <lukas007c@seznam.cz>
2024-09-24 13:00:35 +0200

    drobne upravy

--- Program.cs
+++ Program.cs
@@ -8,9 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-// Pøidání HttpClient s základní adresou (pokud je potøeba)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-// Pokud používáte serverovou aplikaci, nemusí být potøeba WebAssemblyHostBuilder
 
 await builder.Build().RunAsync();
