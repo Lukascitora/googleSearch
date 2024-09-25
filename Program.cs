a3c2b06d352a8a5940446a8f0bb1fd38abbaa8c6
lukas citora <lukas007c@seznam.cz>
2024-09-25 12:23:12 +0200

    final

--- Program.cs
+++ Program.cs
@@ -10,4 +10,5 @@ builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnC
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
 await builder.Build().RunAsync();
